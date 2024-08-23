2d5ff9c202f5fc9ba6a09de71c2ea6b137a885e2
DearNoodle <wan619395@gmail.com>
2024-08-23 21:28:30 +0800

    feat: fixing __dirname undefined problem

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import path from 'path';
 import router from './routes/router.js';
 
 const app = express();
-const __dirname = import.meta.dirname;
+const __dirname = process.cwd();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
